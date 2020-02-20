1ba2c166a8df5888a30496d1ae49ae86bf6ae02e
Dakota Britton <55359304+Dbrit@users.noreply.github.com>
2020-02-19 22:00:38 -0600

    all tasks complete

--- main.s
+++ main.s
@@ -43,7 +43,9 @@ GPIO_LOCK_KEY      EQU 0x4C4F434B  ; Unlocks the GPIO_CR register
 SYSCTL_RCGCGPIO_R  EQU 0x400FE608
 
 CYCLE_TIME EQU 8000000
+CYCLE_TIME_100Hz EQU 160000
 
+;SIN_WAVE DCD 0,631,2513,5618,9895,15279,21683,29006,37134,45938,55279,65009,74977,85023,94991,104721,114062,122866,130994,138317,144721,150105,154382,157487,159369,160000,159369,157487,154382,150105,144721,138317,130994,122866,114062,104721,94991,85023,74977,65009,55279,45938,37134,29006,21683,15279,9895,5618,2513,631
 
        IMPORT  TExaS_Init
        THUMB
@@ -75,22 +77,83 @@ Start
     ORR R4, #3<<1
     STR R4, [R3]
 
+    LDR R3, =GPIO_PORTF_LOCK_R
+    LDR R4, =0x4C4F434B
+    STR R4, [R3]
+    LDR R3, =GPIO_PORTF_CR_R
+    LDR R4, [R3]
+    ORR R4, #1<<4
+    STR R4, [R3]
+    LDR R3, =GPIO_PORTF_DEN_R
+    LDR R4, [R3]
+    ORR R4, #1<<4
+    STR R4, [R3]
     LDR R3, =GPIO_PORTF_DIR_R
     LDR R4, [R3]
     BIC R4, #1<<4
     STR R4, [R3]
-    LDR R3, =GPIO_PORTF_DEN_R
+    LDR R3, =GPIO_PORTF_PUR_R
     LDR R4, [R3]
-    ORR R4, #3
+    ORR R4, #1<<4
     STR R4, [R3]
+
+    LDR R7, =4000000;2400000 "on" time
 
      CPSIE  I    ; TExaS voltmeter, scope runs on interrupts
 loop  
 ; main engine goes here
     
-    LDR R0, =2000000
+    
+    BL checkButton1
+    BL checkButton2
+    
+    MOV R0, R7
     BL run_cycle
-     B    loop
+    B    loop
+
+checkButton1
+    PUSH {R4, R5, R6, LR}
+    LDR R4, =GPIO_PORTE_DATA_R
+    LDR R5, [R4]
+    AND R5, #2
+    CMP R5, #0
+    BHI buttonPressed
+    POP{R4,R5,R6,LR}
+    BX LR
+buttonPressed
+    LDR R4, =GPIO_PORTE_DATA_R
+    MOV R5, #0
+    STR R5, [R4]
+    BL incrementDutyCycle
+    BL waitButton1Release
+    POP {R4, R5, R6, LR}
+    BX LR
+    
+    
+incrementDutyCycle
+    PUSH {R4,R5}
+    LDR R4, =1600000
+    ADD R7, R4
+    LDR R4, =8000000
+    CMP R7, R4
+    BHI reset
+    POP {R4,R5}
+    BX LR
+reset
+    LDR R7, =800000
+    POP {R4,R5}
+    BX LR
+
+waitButton1Release
+    PUSH {R4,R5}
+loopButton1Release
+    LDR R4, =GPIO_PORTE_DATA_R
+    LDR R5, [R4]
+    AND R5, #2
+    CMP R5, #0
+    BHI loopButton1Release
+    POP {R4,R5}
+    BX LR
 
 ; R0 has delay
 wait
@@ -101,23 +164,90 @@ wait
 
 ; R0 has "on" time
 run_cycle
-    PUSH {R3, R4, R5, LR}
+    PUSH {R6, R4, R5, LR}
 
-    LDR R3, =CYCLE_TIME
-    SUB R4, R3, R0      ; R4 is "off" time
+    LDR R6, =CYCLE_TIME
+    SUB R4, R6, R0      ; R4 is "off" time
     
-    LDR R3, =GPIO_PORTE_DATA_R  ; ON
+    LDR R6, =GPIO_PORTE_DATA_R  ; ON
     MOV R5, #4
-    STR R5, [R3]
+    STR R5, [R6]
     BL wait
                        
-    LDR R3, =GPIO_PORTE_DATA_R  ; OFF
-    STR R4, [R3]
+    LDR R6, =GPIO_PORTE_DATA_R  ; OFF
+    MOV R5, #0
+    STR R5, [R6]
     MOV R0, R4
     BL wait    
     
-    POP {LR, R5, R4, R3}
+    POP {LR, R5, R4, R6}
     BX LR
+    
+checkButton2
+    PUSH {R4, R5, R7, LR}
+    LDR R4, =GPIO_PORTF_DATA_R
+    LDR R5, [R4]
+    AND R5, #1<<4
+    CMP R5, #1
+    BLO buttonPressed2
+    POP{R4,R5,R6,LR}
+    BX LR
+buttonPressed2
+    LDR R4, =GPIO_PORTE_DATA_R
+    MOV R5, #0
+    STR R5, [R4]
+    LDR R7, =16000
+    LDR R6, =1000
+    PUSH {R6, R4, R5, LR}
+    BL breathe
+    POP {R6,R4,R5,LR}
+    POP {R4, R5, R7, LR}
+    BX LR
+
+breathe
+    MOV R0, R7
+    LDR R4, =GPIO_PORTF_DATA_R
+    LDR R5, [R4]
+    AND R5, #1<<4
+    CMP R5, #1
+    BLO keepBreathing
+    BX LR
+keepBreathing
+    ADD R7, R7, R6
+    LDR R4, =160000
+    CMP R7, R4
+    BHI reset1
+    PUSH {LR, R11}
+    BL run_cycle_100Hz
+    POP {LR, R11}
+    B breathe
+reset1
+    LDR R7, =16000
+    PUSH {LR, R11}
+    BL run_cycle_100Hz
+    POP {LR, R11}
+    B breathe
+
+; R0 has "on" time
+run_cycle_100Hz
+    PUSH {LR, R5, R4, R6}
+    LDR R6, =CYCLE_TIME_100Hz
+    SUB R4, R6, R0      ; R4 is "off" time
+    
+    LDR R6, =GPIO_PORTE_DATA_R  ; ON
+    MOV R5, #4
+    STR R5, [R6]
+    BL wait
+                       
+    LDR R6, =GPIO_PORTE_DATA_R  ; OFF
+    MOV R5, #0
+    STR R5, [R6]
+    MOV R0, R4
+    BL wait    
+    
+    POP {LR, R5, R4, R6}
+    BX LR
+    
+    
      ALIGN      ; make sure the end of this section is aligned
      END        ; end of file
-
